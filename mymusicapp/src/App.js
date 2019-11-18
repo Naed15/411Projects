9d5cbebd9677b371470b8159c5bef7575fa72601
nae deshazer <naedeshazer@naes-MBP.attlocal.net>
2019-11-17 19:48:36 -0600

    CantGetLoginToFunctionCorrect

--- mymusicapp/src/App.js
+++ mymusicapp/src/App.js
@@ -1,26 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button, TextField } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import Dashboard from "./Dashboard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const useStyles = makeStyles(theme => ({
+	root: {
+		flexGrow: 1
+	},
+	menuButton: {
+		marginRight: theme.spacing(2)
+	}
+}));
+
+function DenseAppBar() {
+	const classes = useStyles();
+	return (
+		<div className={classes.root}>
+			<AppBar position="static">
+				<Toolbar variant="dense">
+					<IconButton
+						edge="start"
+						className={classes.menuButton}
+						color="inherit"
+						aria-label="menu"
+					></IconButton>
+					<Typography variant="h6" color="inherit">
+						Photos
+					</Typography>
+				</Toolbar>
+			</AppBar>
+		</div>
+	);
 }
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			isLoggedIn: false
+		};
+	}
+
+	doLogIn = () => {
+		this.setState({ isLoggedIn: true });
+	};
+//get react router here. then in Router.js import components Login & Dashbaord
+	render() {
+		return (
+			<div>
+			<DenseAppBar />
+				<TextField label="Username *" />
+				<br />
+				<TextField label="Password * " />
+				<br />
+				{this.state.isLoggedIn ? (
+			<Dashboard />
+				) : (
+				<LoginForm doLogIn={this.doLogIn} />
+				)}
+			</div>
+		);
+	}
+}
+
+function LoginForm(props) {
+	return <button onClick={props.doLogIn}>click me</button>;
+}
+// function App() {
+// return (
+// <div className="App">
+// <UserGreeting />
+
+// <DenseAppBar/>
+
+// <TextField label="Enter some text" />
+// <br></br>
+// <TextField label="Enter some text" />
+// <br></br>
+// <Button variant="contained" color="secondary">
+// Submit
+// </Button>
+
+// <Dashboard/>
+// </div>
+// );
+// }
 
 export default App;
---
+++ mymusicapp/src/Dashboard.js
@@ -0,0 +1,107 @@
+import React, {Component} from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+
+const useStyles = makeStyles({
+card: {
+maxWidth: 345,
+},
+media: {
+height: 140,
+},
+});
+
+// function Bar() {
+// return (
+// <div className="App">
+
+
+// <DenseAppBar/>
+
+// <TextField label="Enter some text" />
+// <br></br>
+// <TextField label="Enter some text" />
+// <br></br>
+// <Button variant="contained" color="secondary">
+// Submit
+// </Button>
+
+// <Dashboard/>
+// </div>
+// );
+// }
+
+
+// function DenseAppBar() {
+// const classes = useStyles();
+// return (
+// <div className={classes.root}>
+// <AppBar position="static">
+// <Toolbar variant="dense">
+// <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+// </IconButton>
+// <Typography variant="h6" color="inherit">
+// Photos
+// </Typography>
+// </Toolbar>
+// </AppBar>
+// </div>
+// );
+// }
+
+
+function MediaCard() {
+const classes = useStyles();
+
+return (
+<Card className={classes.card}>
+<CardActionArea>
+<CardMedia
+className={classes.media}
+image="/static/images/cards/contemplative-reptile.jpg"
+title="Contemplative Reptile"
+/>
+<CardContent>
+<Typography gutterBottom variant="h5" component="h2">
+Lizard
+</Typography>
+<Typography variant="body2" color="textSecondary" component="p">
+Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+across all continents except Antarctica
+</Typography>
+</CardContent>
+</CardActionArea>
+<CardActions>
+<Button size="small" color="primary">
+Share
+</Button>
+<Button size="small" color="primary">
+Learn More
+</Button>
+</CardActions>
+</Card>
+);
+}
+function Dashboard() {
+return (
+<div>
+
+<div>Welcome User</div>
+<form>
+<MediaCard/>
+<MediaCard/>
+<MediaCard/>
+</form>
+</div>
+
+
+);
+}
+export default Dashboard;
