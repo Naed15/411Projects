7f75770107009a2ae352384ee84cb197686d61d9
nae deshazer <naedeshazer@naes-MBP.attlocal.net>
2019-11-24 15:52:43 -0600

    done

--- punk-api-react-app/src/App.js
+++ punk-api-react-app/src/App.js
@@ -1,43 +1,45 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import { Component } from "react";
+import React from "react";
 import "./App.css";
+import ThumbUp from "./components/thumbUp";
+import ThumbDown from "./components/thumbDown";
+import Comments from "./components/comments";
 
 class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			isLoading: true,
 			beers: []
 		};
 	}
 
-	componentDidMount() {
-		this.fetchData();
-	}
-
-	fetchData() {
+	btnClick = () => {
 		fetch("https://api.punkapi.com/v2/beers")
-			.then(response => response.json())
-			.then(parsedJSON => console.log(parsedJSON.map(beer => beer.name)))
-			.then(parsedJSON =>
-				parsedJSON.map(beer => ({
-					name: `${beer.name}`,
-					tagline: `${beer.tagline}`,
-					abv: `${beer.abv}`,
-					ibu: `${beer.ibu}`,
-					mash_temp_value_c: `${beer.method.mash_temp.temp.value}`
-				}))
-			)
-			.catch(error => console.log("parsing failed", error));
-	}
+			.then(res => res.json())
+			.then(data => {
+				this.setState({ beers: data });
+			});
+	};
+
+	like = beer => {
+		console.log(beer);
+	};
 
 	render() {
-		// return (console.log('hi'))
-		const { isLoading, beers } = this.state;
 		return (
-			<div>
-				<header>Good Beers</header>
-				<div className={`content ${isLoading ? "is-loading" : ""} `}></div>
+			<div clasName="App">
+				<button onClick={this.btnClick}>All Beers Ya Dirty Animal!</button>
+				{this.state.beers.map(beer => {
+					return (
+						<div key={beer.id}>
+							<p className="description">{beer.description}</p>
+							<img src={beer.image_url} alt="bottles of beer" />
+							<ThumbUp />
+							<ThumbDown />
+							<Comments />
+						</div>
+					);
+				})}
 			</div>
 		);
 	}
