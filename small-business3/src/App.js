1c18b58784de650a366ecdfd684a5ea2f854111a
nae deshazer <naedeshazer@naes-MBP.attlocal.net>
2019-12-11 14:32:52 -0600

    first commit

---
+++ small-business3/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Provider } from "react-redux";
+import store from "./Redux/store";
+import Router from "./Router";
+import Nav from "./Components/Nav";
+import { BrowserRouter } from "react-router-dom";
+
+import "./App.css";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Nav />
+        <Router />
+      </BrowserRouter>
+    </Provider>
+  );
+}
+
+export default App;
---
+++ small-business3/src/Router.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Login from "./Components/Login";
+import Listings from "./Components/Listing";
+import AddListing from "./Components/Add";
+import cookie from "cookie";
+
+const checkAuth = () => {
+  const cookies = cookie.parse(document.cookie);
+
+  return cookies["loggedIn"] ? true : false;
+};
+
+const ProtectedRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        checkAuth() ? <Component {...props} /> : <Redirect to="/" />
+      }
+    />
+  );
+};
+
+const Router = () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={Login} />
+      <Route path="/listings" component={Listings} />
+      <ProtectedRoute path="/add" component={AddListing} />
+    </Switch>
+  );
+};
+export default Router;
