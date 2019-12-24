b988f2fb0059c6a63038962dc244ebf363266687
nae deshazer <naedeshazer1@gmail.com>
2019-12-24 15:01:58 -0600

    newchangessofar

--- random-user-app/src/App.js
+++ random-user-app/src/App.js
@@ -1,4 +1,39 @@
 import React, { Component } from "react";
+import './App.css';
+
+
+
+function UserCard(props) {
+	const cStyle = {
+	listStyleType: "none"
+	}
+
+	let emailInfo = (
+	<li style={cStyle}>{"Emial: " + props.user.email}</li>
+	);
+
+	let gender = (
+	<li style={cStyle}>{"Gender: " + props.user.gender}</li>
+	);
+	  let age = (
+	<li style={cStyle}>{"Age: " + props.user.dob.age + " years old"}</li>
+  );
+ let phone = (
+ <li style={cStyle}>{props.user.phone}</li>
+);
+	
+let location = (
+<li style={cStyle}>{"From: " + props.user.location.street + ", " + props.user.location.city}</li>
+);
+
+let buttonText
+
+
+
+
+
+
+
 
 class App extends Component {
 	constructor(props) {
@@ -17,18 +52,49 @@ class App extends Component {
 		fetch("https://randomuser.me/api?results=25")
 			.then(response => response.json())
 			.then(parsedJSON => console.log(parsedJSON.results(peers => peers.name)))
-			.then(peers => {
-				this.setState({ peers: Data.peers });
+			.then(results.peers => {
+				this.setState({ peers: results.peers });
 			});
 	}
 
 	render() {
     return console.log(this.state.peers);
   }
-  
+  handleClick = (index) => {
+    const newArr = this.state.isHidden.map((item, newIndex) => {
+      if (newIndex === index) {
+        if (item === true) {
+          return false;
+        } else {
+          return true;
+        }
+      } else {
+        return item;
+      }
+    })
+
+    this.setState({
+      isHidden: newArr,
+    })
+  }
 
 
-  
+  render() {
+    return (
+      <div className="App">
+        {this.state.results.map((user, index) =>
+          <UserCard
+            key={index}
+            index={index}
+            user={user}
+            handleClick={this.handleClick}
+            isHidden={this.state.isHidden} />
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
